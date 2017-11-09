6b06958b27e9c482b299e3f54124cbdaec91d509
Jimmy D. Bodden Pineda <jbjdbodden2@gmail.com>
2017-11-09 12:52:55 -0500

    Changed Deploy Script

--- deploy_git_heroku.sh
+++ deploy_git_heroku.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 echo "Updating all Git and Heroku things..."
+
 git add .
-git commit -m "Updating server.js"
+
+if [ "$1" != "" ]; then
+    git commit -m "$1"
+else
+    git commit -m "Latest Commit"
+fi
+
 git push origin master
 git push heroku master
---
+++ testparams.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Updating all Git and Heroku things..."
+
+if [ "$1" != "" ]; then
+    echo "Got this" "$1"
+else
+    echo "Got Nothing"
+fi
